18f5724ecf6b5ca463b563b64cbd3ddb370bacc4
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-11-13 12:21:31 +0100

    📓 Added language class
    - added language class
    - added logo

---
+++ lib/models/language.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'language.g.dart';
+
+@JsonSerializable()
+class Data {
+  @JsonKey(name: 'type')
+  final String type;
+  @JsonKey(name: 'url')
+  final String url;
+@JsonKey(name: 'data')
+  final Map data;
+  
+  Data({this.type, this.url, this.data});
+
+  factory Data.fromJson(Map<String, dynamic> json) => _$LanguageFromJson(json);
+  Map<String, dynamic> toJson() => _$LanguageToJson(this);
+}
--- lib/models/language.g.dart
+++ lib/models/language.g.dart
@@ -1,19 +1,19 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'languages.dart';
+part of 'language.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Data _$LanguagesFromJson(Map<String, dynamic> json) {
+Data _$LanguageFromJson(Map<String, dynamic> json) {
   return Data(
     type: json['type'] as String,
     url: json['url'] as String,
     data: json['data'] as Map,);
 }
 
-Map<String, dynamic> _$LanguagesToJson(Data instance) => <String, dynamic>{
+Map<String, dynamic> _$LanguageToJson(Data instance) => <String, dynamic>{
   'type': instance.type,
   'url': instance.url,
   'data': instance.data,
